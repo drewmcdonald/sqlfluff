c5e2cfcd4bb19123381af1209d386a35ceccad1c
NiallRees <niall@niallrees.com>
2021-01-16 14:45:00 +0000

    Splitting rules into separate files

---
+++ src/sqlfluff/core/rules/std/__init__.py
@@ -0,0 +1,17 @@
+# No docstring here as it would appear in the rules docs.
+# Rule definitions for the standard ruleset, dynamically imported from the directory.
+# noqa
+
+import os
+from glob import glob
+
+rules_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "L*.py")
+
+__all__ = []
+
+for module in sorted(glob(rules_path)):
+    # Manipulate the module path to extract the filename without the .py
+    rule = module.split("/")[-1][:-3]
+    cmd = f"from sqlfluff.core.rules.std.{rule} import Rule_{rule}"
+    __all__.append(f"Rule_{rule}")
+    exec(cmd)
